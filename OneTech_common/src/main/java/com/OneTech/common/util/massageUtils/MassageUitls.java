0ed2eb67b8460c114214ce7dfeffa358a71792a7
stephen21123 <779833570@qq.com>
2019-12-01 17:16:26 +0800

    新增短信验证 密码登陆

---
+++ OneTech_common/src/main/java/com/OneTech/common/util/massageUtils/MassageUitls.java
@@ -0,0 +1,99 @@
+package com.OneTech.common.util.massageUtils;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import com.OneTech.common.util.massageUtils.massage.CHttpPost;
+import com.OneTech.common.util.massageUtils.massage.ConfigManager;
+import com.OneTech.common.util.massageUtils.massage.Message;
+import org.springframework.data.redis.core.TimeoutUtils;
+import org.springframework.stereotype.Service;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@Service("MassageUitls")
+public class MassageUitls {
+    private  SimpleDateFormat sdf = new SimpleDateFormat("MMddHHmmss");
+    @Value("${userid}")
+    String userid;
+    // 用户密码
+    @Value("${pwd}")
+    String pwd;
+    //apiKey
+    @Value("${apiKey}")
+    String apiKey;
+    //地址
+    @Value("${masterIpAddress}")
+    String masterIpAddress;
+    @Value("${ipAddress1}")
+    String ipAddress1;
+    String ipAddress2 = null;
+    String ipAddress3 = null;
+    @Autowired
+    RedisTemplate redisTemplate;
+    public Boolean sendMassageToSingle(JSONObject requestJson){
+        ConfigManager.setIpInfo(masterIpAddress, ipAddress1, ipAddress2, ipAddress3);
+        //密码是否加密   true：密码加密;false：密码不加密
+        ConfigManager.IS_ENCRYPT_PWD = true;
+        boolean isEncryptPwd = ConfigManager.IS_ENCRYPT_PWD;
+        try {
+            // 参数类
+            Message message = new Message();
+            // 实例化短信处理对象
+            CHttpPost cHttpPost = new CHttpPost();
+            // 设置账号   将 userid转成大写,以防大小写不一致
+            message.setUserid(userid.toUpperCase());
+            //判断密码是否加密。
+            //密码加密，则对密码进行加密
+            if (isEncryptPwd) {
+                // 设置时间戳
+                String timestamp = sdf.format(Calendar.getInstance().getTime());
+                message.setTimestamp(timestamp);
+                // 对密码进行加密
+                String encryptPwd = cHttpPost.encryptPwd(message.getUserid(), pwd, message.getTimestamp());
+                // 设置加密后的密码
+                message.setPwd(encryptPwd);
+            } else {
+                // 设置密码
+                message.setPwd(pwd);
+            }
+            // 设置手机号码 此处只能设置一个手机号码
+            message.setMobile(requestJson.getString("phone"));
+            message.setApikey(apiKey);
+            // 设置内容
+            Random r = new Random();
+            int code = r.nextInt(899999)+100000;
+            redisTemplate.opsForValue().set(requestJson.getString("phone"),code,60,TimeUnit.SECONDS);
+            message.setContent("您的验证码是"+code+"，在1分钟内有效。如非本人操作请忽略本短信。");
+            // 设置扩展号
+            message.setExno("11");
+            // 用户自定义流水编号
+            message.setCustid(String.valueOf(code));
+            // 自定义扩展数据
+            message.setExdata("abcdef");
+            //业务类型
+            message.setSvrtype("SMS001");
+            // 返回的平台流水编号等信息
+            StringBuffer msgId = new StringBuffer();
+            // 返回值
+            int result = -310099;
+            // 发送短信
+            result = cHttpPost.singleSend(message, msgId);
+            // result为0:成功;非0:失败
+            if (result == 0) {
+                System.out.println("单条发送提交成功！");
+                System.out.println(msgId.toString());
+                return true;
+            } else {
+                System.out.println("单条发送提交失败,错误码：" + result);
+                return false;
+            }
+        } catch (Exception e) {
+            //异常处理
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- OneTech_web/src/main/java/com/OneTech/web/controller/mainController.java
+++ OneTech_web/src/main/java/com/OneTech/web/controller/mainController.java
@@ -2,54 +2,106 @@ package com.OneTech.web.controller;
 
 import com.OneTech.common.constants.SystemConstants;
 import com.OneTech.common.controller.CommonController;
+import com.OneTech.common.util.BooleanUtils;
 import com.OneTech.common.util.UUIDUtils;
+import com.OneTech.common.util.massageUtils.MassageUitls;
 import com.OneTech.common.vo.StatusBean;
 import com.OneTech.model.model.UserInfoBean;
 import com.OneTech.service.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Date;
+
 
 @RestController
 @RequestMapping(value = "/")
 public class mainController extends CommonController {
     @Autowired
     UserInfoService userInfoService;
+    @Autowired
+    RedisTemplate redisTemplate;
+    @Autowired
+    MassageUitls massageUitls;
+    /**
+     * 获取验证码
+     */
+    @PostMapping("getVerifiCode")
+    public StatusBean<?> getVerifiCode() {
+        StatusBean<UserInfoBean> statusBean = new StatusBean<>();
+        if(massageUitls.sendMassageToSingle(getRequestJson())){
+            statusBean.setRespMsg("发送成功");
+            statusBean.setRespCode(SystemConstants.RESPONSE_SUCCESS);
+        }
+        else{
+            statusBean.setRespMsg("发送失败");
+            statusBean.setRespCode(SystemConstants.RESPONSE_FAIL);
+        }
+        return statusBean;
+    }
 
     /**
      * 用户登录
+     *
      * @return
      */
     @PostMapping(value = "login")
-    public StatusBean<?> login(){
+    public StatusBean<?> login() {
         StatusBean<UserInfoBean> statusBean = new StatusBean<>();
         UserInfoBean userInfo = new UserInfoBean();
         UserInfoBean userInfoBean;
         try {
             String phone = getRequestJson().getString("phone");
-            userInfo.setPhone(phone);
-            userInfoBean = userInfoService.selectOne(userInfo);
-            if (userInfoBean == null) {
-                userInfo.setId(UUIDUtils.getRandom32());
-                userInfo.setUserName("新用户");
-                userInfo.setWechatId(UUIDUtils.getRandom32());
-                userInfo.setMomentsId(UUIDUtils.getRandom32());
-                userInfo.setImgPath("img/head.png");
-                userInfo.setBackgroundImg("img/background.png");
-                userInfo.setCreateTime(new Date());
-                userInfoService.save(userInfo);
-                userInfoBean = userInfo;
+            String loginType = getRequestJson().getString("loginType");
+            if("password".equals(loginType)){
+                String password = getRequestJson().getString("password");
+                userInfo.setPhone(phone);
+                userInfo.setPassWord(password);
+                userInfoBean = userInfoService.selectOne(userInfo);
+                if (userInfoBean == null) {
+                    statusBean.setRespCode(SystemConstants.RESPONSE_FAIL);
+                    statusBean.setRespMsg("密码不正确");
+                    return statusBean;
+                }else{
+                    statusBean.setRespCode(SystemConstants.RESPONSE_SUCCESS);
+                    statusBean.setRespMsg("登陆成功!");
+                    statusBean.setData(userInfoBean);
+                    return statusBean;
+                }
             }
-        }catch (Exception e){
+            String verifiCode = getRequestJson().getString("verifiCode");
+            if(BooleanUtils.isEmpty(redisTemplate.opsForValue().get(phone))){
+                statusBean.setRespCode(SystemConstants.RESPONSE_FAIL);
+                statusBean.setRespMsg("请重新发送验证码");
+                return statusBean;
+            }
+            if(redisTemplate.opsForValue().get(phone).toString().equals(verifiCode)) {
+                userInfo.setPhone(phone);
+                userInfoBean = userInfoService.selectOne(userInfo);
+                if (userInfoBean == null) {
+                    userInfo.setId(UUIDUtils.getRandom32());
+                    userInfo.setUserName("新用户");
+                    userInfo.setWechatId(UUIDUtils.getRandom32());
+                    userInfo.setMomentsId(UUIDUtils.getRandom32());
+                    userInfo.setImgPath("img/head.png");
+                    userInfo.setBackgroundImg("img/background.png");
+                    userInfo.setCreateTime(new Date());
+                    userInfoService.save(userInfo);
+                    userInfoBean = userInfo;
+                }
+                statusBean.setRespCode(SystemConstants.RESPONSE_SUCCESS);
+                statusBean.setRespMsg("登陆成功!");
+                statusBean.setData(userInfoBean);
+            }
+            else{
+                statusBean.setRespCode(SystemConstants.RESPONSE_FAIL);
+                statusBean.setRespMsg("验证码不正确");
+            }
+        } catch (Exception e) {
             e.printStackTrace();
             statusBean.setRespCode(SystemConstants.RESPONSE_FAIL);
-            statusBean.setRespMsg("登陆失败!"+e);
-            return statusBean;
+            statusBean.setRespMsg("登陆失败!" + e);
         }
-        statusBean.setRespCode(SystemConstants.RESPONSE_SUCCESS);
-        statusBean.setRespMsg("登陆成功!");
-        statusBean.setData(userInfoBean);
         return statusBean;
     }
 }
